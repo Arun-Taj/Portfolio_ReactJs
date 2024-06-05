1cb5e0506184780ea274b9c6000de9f312d3fd81
Arun Tajpuriya <aruntajpuriya1@gmail.com>
2024-06-06 00:55:35 +0545

    Minor Changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 '>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row '>
             <div className='flex flex-col justify-center h-full'>
-                <h2 className='text-4xl sm:text-7xl  font-bold text-white  duration-200 hover:scale-105 pt-10'>Software Engineer</h2>
+                <h2 className='text-4xl sm:text-7xl  font-bold text-white  duration-200 hover:scale-105 pt-10'>Front-End Web Developer</h2>
                 <p className='text-gray-500 py-4 max-w-md'>
                 
                 </p>
