f5a4ab0bf528f7fc3a40c0d35528d0359e84dec8
Arun Tajpuriya <aruntajpuriya1@gmail.com>
2024-06-13 17:27:38 +0545

    adjusted navbar

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -25,8 +25,8 @@ const Portfolio = () => {
   ]
   return (
    <>
-    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white '>
-      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white pt-20'>
+      <div className='max-w-screen-lg p-4  mx-auto flex flex-col justify-center w-full h-full'>
         <div className='pb-8  '>
           <p className='text-4xl font-bold inline border-b-4 border-gray-500 '>Portfolio</p>
           <p className='py-6 '>These are just demos...Actual works are yet to add on profile</p>
